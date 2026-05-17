e681b104aede25463f400d47c77c6ee0264d7572
DikryRamdani <rmdnidkry@gmail.com>
2026-05-17 15:34:14 +0700

    feat: implement responsive Navbar component and update site title

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -86,7 +86,7 @@ export default function Navbar() {
             onClick={() => handleNavigation("/")}
           >
             <img
-              src="/favicon.ico"
+              src="/favicon.png"
               alt="Logo Desa Segarjaya"
               width={36}
               height={36}
